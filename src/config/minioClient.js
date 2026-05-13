038a788bfd60e92adc6871121a2fbc34cdfe9fa1
Lakshman-DevOps-25 <laxmanaraog@gmail.com>
2026-05-13 11:46:37 +0530

    Update minioClient.js

--- src/config/minioClient.js
+++ src/config/minioClient.js
@@ -20,6 +20,7 @@ export function getMinioClient() {
     endPoint:  process.env.MINIO_ENDPOINT  || 'localhost',
     port:      parseInt(process.env.MINIO_PORT || '9000'),
     useSSL:    process.env.MINIO_USE_SSL === 'true',
+    pathStyle: true,
     accessKey: process.env.MINIO_ACCESS_KEY || 'minioadmin',
     secretKey: process.env.MINIO_SECRET_KEY || 'minioadmin',
   });
